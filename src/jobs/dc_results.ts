16389b3ab8affb6f84f51579ec2866a4e08e72d1
jamesm2w <11365435+jamesm2w@users.noreply.github.com>
2026-05-07 07:12:07 +0100

    Emoji config for production app

--- src/jobs/dc_results.ts
+++ src/jobs/dc_results.ts
@@ -92,23 +92,23 @@ if (process.env.NODE_ENV != "production") {
 } else {
   // Production Config
   partyIdEmoji = {
-    PP52: "<:Conservative:1500963713547505715>", // Con
-    PP53: "<:Labour:1500963717716770907>", // Lab
+    PP52: "<:Conservative:1500964868197449778>", // Con
+    PP53: "<:Labour:1500964871569932499>", // Lab
     "joint-party:53-119":
-      "<:Labour:1500963717716770907> <:CoOperative:1500963711601610853>", // Lab Co-op
-    PP90: "<:LibDem:1500963719360806942>", // LD
-    PP63: "<:Green:1500963716001435648>", // Grn
-    PP7931: "<:ReformUK:1500963698259267604>", // RefUK
-    PP102: "<:SNP:1500963696506044426>", // SNP
-    PP77: "<:PlaidCymru:1500963699907756112>", // PC
-    PP11382: "<:WPB:1500963691451912302>", // WPB
-    // Aspire
-    // TUSC
-    // SGP
-    // SDP
-    // UKIP
-    // OMRLP
-    // YourParty
+      "<:Labour:1500964871569932499> <:CoOperative:1500964866481983642>", // Lab Co-op
+    PP90: "<:LibDem:1500964873461301329>", // LD
+    PP63: "<:Green:1500964869929697501>", // Grn
+    PP7931: "<:ReformUK:1500964860354363574>", // RefUK
+    PP102: "<:SNP:1500964858601017405>", // SNP
+    PP77: "<:PlaidCymru:1500964861742682295>", // PC
+    PP11382: "<:WPB:1500964851818696754>", // WPB
+    PP6713: "<:Aspire:1500964865081344172>", // Aspire // TODO
+    PP804: "<:TUSC:1500964853089702040>", // TUSC
+    PP130: "<:SGP:1500964855786766366>", // SGP
+    PP243: "<:SDP:1500964850380046408>", // SDP
+    PP85: "<:UKIP:1500964849012965567>", // UKIP
+    PP66: "<:OMRLP:1500964863449628812>", // OMRLP
+    PP18172: "<:YourParty:1500491954020942066>", // YourParty
     other: "⚪", // Other/Inds
   };
 }
